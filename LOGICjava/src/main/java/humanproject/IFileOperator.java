f8bd22f9a98d2e2c95af4b807e7ad1ae4fd593ae
Kamil Stępień <130293368+reavz7@users.noreply.github.com>
2024-07-20 11:39:46 +0200

    Delete LOGICjava directory

--- LOGICjava/src/main/java/humanproject/IFileOperator.java
+++
@@ -1,9 +0,0 @@
-package humanproject;
-
-import java.util.List;
-
-public interface IFileOperator {
-
-    void serialize(List<Human> users, String filename);
-    List<Human> deserialize(String filename);
-}
